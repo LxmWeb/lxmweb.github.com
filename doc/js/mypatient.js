820967fa9bc7015376be487b8228b6a5bc0e22c0
zjm <17816869729@163.com>
2016-06-07 23:45:36 +0800

    字体大小

--- doc/js/mypatient.js
+++ doc/js/mypatient.js
@@ -4,6 +4,7 @@ function mypatientskip(){
         window.location.href="me.html";
 	});
 	$(".pat-object").click(function(){
+		$(this).css("background-color","#f2f2f2");
         window.location.href="object.html";
 	});
 }
